d9aefe955cf84a855c6099ee05cc4d7c99124004
hishnick5 <hishnick5@mail.ru>
2026-04-11 23:25:17 +0300

    2

--- PYTHON-12/hello.py
+++ PYTHON-12/hello.py
@@ -1,1 +1,9 @@
-print("Hello World! ")
+number = int(input("Введите число "))
+num_sum = 0
+
+while number != 0:
+    last_num = number % 10
+    num_sum += last_num
+    number //= 10
+
+print("num", number, "sum", num_sum)
